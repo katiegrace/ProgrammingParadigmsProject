349690c4bbdd45f9b1e91c49e4bbae8a25fd3128
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2022-11-14 20:12:48 -0500

    Added classes to models.py

--- src/project/TinDev/models.py
+++ src/project/TinDev/models.py
@@ -2,3 +2,34 @@ from django.db import models
 #hi
 
 # Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.CharField(max_length=500)
+    zipcode = models.TextField()
+    skills = models.CharField(max_length=500)
+    git_username = models.CharField(max_length=200)
+    years_exp = models.TextField()
+    username =  models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"Name:{self.name}\nBio: {self.bio}"
+
+class Recruiter(models.Model):
+    name = models.CharField(max_length=200)
+    company = models.CharField(max_length=300)
+    zipcode = models.TextField()
+    username =  models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"Name:{self.name}\Company: {self.company}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return f"{self.title} by {self.author_name} ({self.pub_date})"
