4e63c9498dd5b027c8b6cda76d827966123825cb
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2022-11-14 20:58:56 -0500

    admin thingy
    admin thingy

--- src/project/TinDev/admin.py
+++ src/project/TinDev/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Post, Candidate, Recruiter
+
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Candidate)
+admin.site.register(Recruiter)
