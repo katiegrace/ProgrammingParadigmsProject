4478c4d6d0e6829250ed658371194803c4068729
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2022-11-15 21:16:46 -0500

    Update urls.py

--- src/project/project/urls.py
+++ src/project/project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('TinDev', include('TinDev.urls')),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', include('login.urls')),
 ]
