c319d8747c83e87b30e1d45ee8b7ad4affbd30e9
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2022-11-15 21:26:56 -0500

    Fixes

--- src/project/TinDev/urls.py
+++ src/project/TinDev/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name='TinDev'
 
 urlpatterns = [
-    #path('',views.IndexView.as_view(), name='index'),
+    path('',views.IndexView.as_view(), name='index'),
     #path('<int:pk>/',views.PostDetailView.as_view(), name='detail'),
 ]
