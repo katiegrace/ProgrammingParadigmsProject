e531107cc0d81f9e43b9f793e6ed84158a10ce22
sarahcullinan <scullin2@nd.edu>
2022-11-15 21:00:46 -0500

    first attempt at Profile creation

--- src/project/TinDev/models.py
+++ src/project/TinDev/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-#hi
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User #add this
+from django.contrib.auth.models import User
+from django.dispatch import receiver #add this
+from django.db.models.signals import post_save #add this
+
 
 # Create your models here.
+class Profile(models.Model):   #add this class and the following fields
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	products = models.ManyToManyField(Product)
+
+    @receiver(post_save, sender=User) #add this
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+			Profile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User) #add this
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
+
 class Candidate(models.Model):
     name = models.CharField(max_length=200)
     bio = models.CharField(max_length=500)
