a2767f506f365a7d80521d904a5264421242036b
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2022-11-16 20:16:15 -0500

    restarting

---
+++ src/TinDev/project/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+app_name = 'project'  # creates a namespace for this app
+urlpatterns = [
+   #path('', views.feature1, name='index'), # the path for our index view
+   #path('<int:blog_id>/', views.feature2, name='detail')
+]
