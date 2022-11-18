f0e6b5f8753210a6c1214addfdd0ddb781f2bd15
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2022-11-18 12:41:53 -0500

    Trying login and logout

--- src/TinDev/project/urls.py
+++ src/TinDev/project/urls.py
@@ -15,13 +15,19 @@ urlpatterns = [
    #path('<int:blog_id>/', views.feature2, name='detail')
 ]
 '''
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', views.index ),  #the path for our index view
     path('candidateProfile/', views.candidateProfile, name='CandidateProfile'),
     path('recruiterProfile/', views.recruiterProfile, name='RecruiterProfile'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout')
+    #path('login/', views.login, name='login'),
+    #path('logout/', views.logout, name='logout'),
+    #re_path(r'^$', TemplateView.as_view(template_name='base.html'), name='base'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
