6982efb6936b1980a2fede9d1809c80ff1d02eba
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2022-12-03 14:13:19 -0500

    trying dropdown

--- src/TinDev/project/admin.py
+++ src/TinDev/project/admin.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from .models import CandidateProfile
 from .models import RecruiterProfile
 
+
 admin.site.register(CandidateProfile)
 
 admin.site.register(RecruiterProfile)
+
