f6b7147549d40a74eeee4f9e094684cbf8f3dee2
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2022-12-04 18:58:50 -0500

    Trying to get filter to work

--- src/TinDev/project/static/filter.js
+++ src/TinDev/project/static/filter.js
@@ -14,7 +14,7 @@ function applyPositionFilter() {
     // Show only active posts
     else if (text == "Active Posts Only") {
         for (let i = 0; i < allPosts.length; i++) {
-            let status = allPosts[i].position_status;
+            let status = allPosts[i].post_status;
             if (status == "Active") {
                 allPosts[i].style.display = "block";
             }
