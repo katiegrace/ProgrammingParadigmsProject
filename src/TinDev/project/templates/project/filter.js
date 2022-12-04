b18f1e5cd57707e5b37aec9b0fb3c9409d83250a
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2022-12-04 14:00:43 -0500

    filter

--- src/TinDev/project/templates/project/filter.js
+++ src/TinDev/project/templates/project/filter.js
@@ -2,11 +2,9 @@
 
 function applyPositionFilter() {
     let selectElement = document.getElementById("filter");
-    // 1, 2 or 3
     let value = selectElement.value;
-    // Assistant Professor, Associate Professor, All
     let text = selectElement.options[selectElement.selectedIndex].text;
-    // all faculty divs
+    // all posts
     let allPosts = document.getElementsById("printfiltered");
     // show all elements
     if (text == "All Posts") {
@@ -14,9 +12,9 @@ function applyPositionFilter() {
             allPosts[i].style.display = "block";
         }
     }
+    // Show only active posts
     else if (text == "Active Posts Only") {
         for (let i = 0; i < allPosts.length; i++) {
-            let h3 = allPosts[i].querySelector("h3");
             let status = allPosts[i].position_status;
             if (status == "Active") {
                 allPosts[i].style.display = "block";
