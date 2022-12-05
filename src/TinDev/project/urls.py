b3b16260c3b8baf265207f90094b2b296f5cd339
sarahcullinan <scullin2@nd.edu>
2022-12-04 19:05:54 -0500

    like/dislike

--- src/TinDev/project/urls.py
+++ src/TinDev/project/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
     path('interestedJobs/', views.CandidateIndexView.as_view(), name='Interest'),
     path('delete/<int:id>/', views.delete, name='Delete'),
     path('edit/<int:id>', views.edit, name='Edit'),
-    path('post_update/', views.RecruiterIndexView.as_view(), name='Update')
+    path('post_update/', views.RecruiterIndexView.as_view(), name='Update'),
+    #to like/dislike a post
+    path('interested', views.interest, name='interest')
+    path('not_interested', views.not_interest, name='not_interest')
     
 ]
