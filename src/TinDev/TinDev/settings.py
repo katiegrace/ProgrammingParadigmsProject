0a3a3989be4c8476a83990dad39eb6302aa25521
celinaryan <cryan27@nd.edu>
2022-12-04 22:45:44 -0500

    recruiter navbar stuff
    it isn't linked right but I wanted to separate this edit from trying to filter out stuff, this can be ignored but would like to get this setup -- navbar for recruiter

--- src/TinDev/TinDev/settings.py
+++ src/TinDev/TinDev/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_admin_listfilter_dropdown',
+   # 'django_admin_listfilter_dropdown',
 ]
 
 
