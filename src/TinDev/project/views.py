96bb09abe9c846cff717c9065f41a196028ddc88
sarahcullinan <scullin2@nd.edu>
2022-12-05 13:02:39 -0500

    filter

--- src/TinDev/project/views.py
+++ src/TinDev/project/views.py
@@ -119,11 +119,24 @@ class CandidateIndexView(ListView):
 class RecruiterIndexView(ListView):
     template_name = 'project/recruiterViewAllPosts.html'
     context_object_name = 'post_list'
+
     def get_queryset(self):
         #filter all the post objects to only include those in which in recruiter's username matched the one logged in
         uname_id = RecruiterProfile.objects.filter(username=self.request.session['logged_user'])[0]
         #need to do return (Post.objects.filter(recruiter.username = uname).order_by('-expiration_date'))
-        return (Post.objects.filter(recruiter= uname_id).order_by('-expiration_date'))
+        q_set = Post.objects.filter(recruiter= uname_id).order_by('-expiration_date')
+
+        filt = request.GET.get('filter')
+        if filt == 'active':
+            q_set = Post.objects.filter(recruiter=uname_id, status="Active").order_by('-expiration_date')
+            #q_set = q_set.filter(status='Active')
+        elif filt == 'inactive':
+            q_set = Post.objects.filter(recruiter=uname_id, status="Inactive").order_by('-expiration_date')
+            #q_set = q_set.filter(status='Inactive')
+        elif filt == 'intersted_cands':
+            q_set = Post.objects.filter(recruiter= uname_id, likes>=1).order_by('-expiration_date')
+
+        return q_set
 
 class CandPostDetailView(DetailView):
     model = Post
@@ -181,22 +194,6 @@ def get_filters(self, request):
         filter_set = ('all', 'active', 'inactive', 'location', 'keywords')
 '''
 
-def recruiterFilter(self, request):
-    #get all the recruiters posts 
-    uname_id = RecruiterProfile.objects.filter(username=self.request.session['logged_user'])[0]
-    q_set = Post.objects.filter(recruiter= uname_id).order_by('-expiration_date')
-
-    filt = request.GET.get('filter')
-    if filt == 'active':
-        q_set = Post.objects.filter(recruiter=uname_id, status="Active").order_by('-expiration_date')
-        #q_set = q_set.filter(status='Active')
-    elif filt == 'inactive':
-        q_set = Post.objects.filter(recruiter=uname_id, status="Inactive").order_by('-expiration_date')
-        #q_set = q_set.filter(status='Inactive')
-    elif filt == 'intersted_cands':
-        q_set = q_set.filter(likes=1)
-
-    return q_set
 
 '''
 elif "all" in request.POST:
