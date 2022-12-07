22fb4de39f9b88c31683b833a57d38907ac24d1b
celinaryan <cryan27@nd.edu>
2022-12-07 15:25:24 -0500

    got Compatability to work and display

--- src/TinDev/project/models.py
+++ src/TinDev/project/models.py
@@ -2,6 +2,48 @@ from django.db import models
 
 # Create your models here.
 
+
+def remove_stopWords(str):
+    k = []
+    words = str.split()
+    z = words
+    with open('/Users/celinaryan/Documents/GitHub/ProgrammingParadigmsProject/src/TinDev/project/templates/project/stopwords.txt', 'r') as f:
+        for word in f:
+            word = word.split('\n')
+            k.append(word[0])
+
+        p = [t for t in z if t not in k]
+    return p
+
+def compatabilityScore(post):
+    likes = post.likes # list of cands that liked the post
+#     #from the post
+    preferred_skills = post.preferred_skills
+    position_title = post.position_title
+    description = post.description
+    post_str_list= position_title + preferred_skills + description
+    post_SW = remove_stopWords(post_str_list) # SW = stopwords
+
+    scores={} # our scores dict that we will return
+
+#     #from cand
+
+    for cand in likes.all():
+        skills = cand.skills
+        bio = cand.bio
+        cand_str_list = bio + skills
+        cand_SW = remove_stopWords(cand_str_list)
+        lenTotal= len(post_SW) + len(cand_SW)
+        counter=0
+        for word in post_SW:
+            for skill in cand_SW:
+                if word==skill:
+                    counter+=1
+                score = round((counter + 7 / lenTotal) * 100,2) # this is an arbitrary multiplication so the number isn't tiny
+                scores[cand]=(score, cand.name)
+
+    return scores
+
 class CandidateProfile(models.Model):
 
     name = models.CharField(max_length=200)
@@ -36,6 +78,14 @@ class Post(models.Model):
     #for interested v. not
     likes = models.ManyToManyField(CandidateProfile, blank=True,related_name='likes')
     dislikes = models.ManyToManyField(CandidateProfile, blank=True, related_name='dislikes')
+    #score = models.ForeignKey(CandidateProfile, on_delete=models.CASCADE, default="")
+
+    @property
+    def scores(self):
+        scr = compatabilityScore(self)
+        #raise NotImplementedError(scr)
+        return scr
+
 
 class Offer(models.Model):
     #a offer is linked to a post, sent from a recruiter, to a candidate
